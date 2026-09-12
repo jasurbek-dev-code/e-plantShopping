1d9414310a626bdb0948d401ef75d74ed8513f1b
Jasur <yoqubjonovjasurbek543@gmail.com>
2026-09-12 09:32:50 -0400

    changes

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,20 +9,28 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total=0
+    cart.forEach((item)=>{
+        total+=parseFloat(item.cost.substring(1))*item.quantity
+    })
+    return total
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e)
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name:item.name,quantity:item.quantity+1}))
   };
-
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
